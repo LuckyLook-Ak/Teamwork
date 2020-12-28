140502a90ec1f7f8a6af24bd4c8af652b4105d3c
Anepsios <Anepsios@users.noreply.github.com>
2020-12-28 18:56:58 +0200

    Add project files.

---
+++ Teamwork/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Teamwork
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Teamwork/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Teamwork.MvcApplication" Language="C#" %>
